e0063765594bccab7180b6fa629af28b52f569b4
andrew tj <andrewtjanadi@gmail.com>
2025-05-05 00:29:07 +0700

    Update Invoice

--- service/app/server/handler/invoice.js
+++ service/app/server/handler/invoice.js
@@ -4,23 +4,38 @@ const router = express.Router();
 
 const list = async (req, res) => {
     try {
-            queryParams = req.query
-    
-            invoices = await invoiceHelper.InvoiceList({
-                filter: JSON.parse(queryParams.filter||'{}'),
-                sort:queryParams.sort,
-                limit: queryParams.limit,
-                page: queryParams.page
-            })
-    
-            res.send(invoices);
-        } catch(err) {
-            console.log(err);
-    
-            res.send({error: "Terjadi kesalahan pada sistem"})
-        }
+        queryParams = req.query
+
+        invoices = await invoiceHelper.InvoiceList({
+            filter: JSON.parse(queryParams.filter||'{}'),
+            sort:queryParams.sort,
+            limit: queryParams.limit,
+            page: queryParams.page
+        })
+
+        res.send(invoices);
+    } catch(err) {
+        console.log(err);
+
+        res.send({error: "Terjadi kesalahan pada sistem"})
+    }
+}
+
+const updateStatus = async(req, res) => {
+    try {
+        body = req.body
+
+        invoice = await invoiceHelper.UpdateInvoiceStatus({invoiceId: req.params.invoiceId, invoiceStatus: body.status})
+
+        res.send(invoice)
+    } catch (error) {
+        console.log(err);
+
+        res.send({error: "Terjadi kesalahan pada sistem"})
+    }
 }
 
 router.get('', list)
+router.patch('/:invoiceId/status', updateStatus)
 
 module.exports = router
--- service/helper/invoice/invoice.js
+++ service/helper/invoice/invoice.js
@@ -25,25 +25,35 @@ module.exports.UpdateInvoiceSentAt = async (invoice) => {
 module.exports.InvoiceList = async ({filter={},sort=[],limit,page}) => {
     pagination = paginationUtil.Paginate({limit,page})
     
-        whereClause = _.map(filter, (v,k) => {
-            return {
-                [k]: {
-                    [Op.eq]: v
-                }
+    whereClause = _.map(filter, (v,k) => {
+        return {
+            [k]: {
+                [Op.eq]: v
             }
-        })
-    
-        if (sort.length == 0) {
-            sort = [['createdAt', 'DESC']]
-        } else {
-            sort = JSON.parse(sort)
         }
-    
-        return Invoice.findAll({
-            where: whereClause,
-            order: sort,
-            limit: pagination.limit,
-            offset: pagination.offset,
-            include: Resident
-        })
+    })
+
+    if (sort.length == 0) {
+        sort = [['createdAt', 'DESC']]
+    } else {
+        sort = JSON.parse(sort)
+    }
+
+    return Invoice.findAll({
+        where: whereClause,
+        order: sort,
+        limit: pagination.limit,
+        offset: pagination.offset,
+        include: Resident
+    })
+}
+
+module.exports.UpdateInvoiceStatus = async ({invoiceId,invoiceStatus}) => {
+    invoice = await Invoice.findOne({id:invoiceId})
+
+    invoice.status=invoiceStatus
+
+    await invoice.save()
+
+    return invoice
 }
