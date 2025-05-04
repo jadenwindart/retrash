d34c647da184a14428adaf27ba2d5dfb75ab78c8
andrew tj <andrewtjanadi@gmail.com>
2025-05-04 20:00:38 +0700

    Config DB

--- service/repository/database.js
+++ service/repository/database.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 db = {}
 
-const sequelize = new Sequelize('retrash', 'postgres', 'postgres', {
+const sequelize = new Sequelize('retrash', process.env.POSTGRES_DB_USERNAME, process.env.POSTGRES_DB_PASSWORD, {
     host: process.env.POSTGRES_DB_HOST,
     port: process.env.POSTGRES_DB_PORT,
     dialect: 'postgres',
