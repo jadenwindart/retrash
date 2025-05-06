9f44c2d1ead22a39df267d1d1b4e012d3e354db6
andrew tj <andrewtjanadi@gmail.com>
2025-05-07 03:32:33 +0700

    Return Response

--- service/app/server/handler/whatsapp.js
+++ service/app/server/handler/whatsapp.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const receiveMessage = async (req, res) => {
     console.log(req.body)
+
+    res.send()
 }
 
 router.post('/v1/message/notification', receiveMessage)
